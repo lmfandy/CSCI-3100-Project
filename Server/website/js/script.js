3abad33904d686ec0d9b0685dffc9e321da7feba
But Chi Hin <chihinbut@gmail.com>
2020-03-27 02:18:57 +0800

    updated html and js

--- Server/website/js/script.js
+++ Server/website/js/script.js
@@ -15,75 +15,97 @@ $(function () {
   $("#createCustomerBtn").click(() => togglePages(2));
   $("#createOwnerBtn").click(() => togglePages(3));
 
-  $(function(){
-    $("#searchNowBtn").click(function(){
-      $("#mainSearchBar").slideDown("slow");
-    });
+  $("#searchNowBtn").click(function () {
+    $("#mainSearchBar").slideDown("slow");
   });
 
-  $(function(){
-    $("#mainSearchBarCloseBtn").click(function(){
-      $("#mainSearchBar").slideUp("slow");;
-    });
+  $("#mainSearchBarCloseBtn").click(function () {
+    $("#mainSearchBar").slideUp("slow");;
   });
 
-  //the login function
-  $("#loginBtn").click(function () {
-    //this is just a sample function.
-    //we will not check the login info using this simple approach.
-    var email = document.getElementById("CustomerLoginEmail").value;
-    var password = document.getElementById("CustomerLoginPassword").value;
-    switch (email) {
-      case "customer":
-        if (password == "123456")
-          window.location.href = "customer_info.html";
-        else
-          alert("ERROR: Incorrect password. Try again.");
-        break;
-      case "owner":
-        if (password == "qwerty")
-          window.location.href = "owner_info.html";
-        else
-          alert("ERROR: Incorrect password. Try again.");
-        break;
-      default:
-        alert("ERROR: The email is not registered. Try again.");
-        break;
-    }
-    return false;
+  $("#searchBtn").click(function () {
+    $("html").animate({ scrollTop: $(searchResult).offset().top }, 1000);
   });
 });
 
-$(function(){
+$(function () {
   var range = document.getElementById("priceRange");
   var output = document.getElementById("priceValue");
-  output.innerHTML = "Each price / hr: $" + (range.value*(20) + 20); // Display the default slider value
+  output.innerHTML = "Each price / hr: $" + (range.value * (20) + 20); // Display the default slider value
 
   // Update the current slider value (each time you drag the slider handle)
-  range.oninput = function() {
-    output.innerHTML = "Each price / hr: $" + (this.value*(20) + 20);
+  range.oninput = function () {
+    output.innerHTML = "Each price / hr: $" + (this.value * (20) + 20);
   }
 });
 
-$(function(){
-  $("#Signup_CustomerSubmitBtn").click(() => {
-    var username = document.getElementById("Signup_CustomerUsername").value;
-    var email = document.getElementById("Signup_CustomerEmail").value;
-    var password = document.getElementById("Signup_CustomerPassword").value;
-    var phone = document.getElementById("Signup_CustomerPhone").value;
-    var customer = {
-      "username": username,
-      "email": email,
-      "password": password,
-      "phone": phone
-    }
-    console.log(customer);
-    $.ajax({
-      type: "POST",
-      async: false,
-      dataType: "json",
-      data: customer,
-      url:"/CustomerSignUp"
-    });
+function login(data) {
+  //this is just a sample function.
+  //we will not check the login info using this simple approach.
+  var username = data.username.value;
+  var password = data.password.value;
+  switch (username) {
+    case "customer":
+      if (password == "123456")
+        window.location.href = "customer_info.html";
+      else
+        alert("ERROR: Incorrect password. Try again.");
+      break;
+    case "owner":
+      if (password == "qwerty")
+        window.location.href = "owner_info.html";
+      else
+        alert("ERROR: Incorrect password. Try again.");
+      break;
+    default:
+      alert("ERROR: The email is not registered. Try again.");
+      break;
+  }
+  return false;
+}
+
+function customerSignUp(data) {
+  var username = data.username.value;
+  var email = data.email.value;
+  var password = data.password.value;
+  var phone = data.phone.value;
+  var customer = {
+    "username": username,
+    "email": email,
+    "password": password,
+    "phone": phone
+  }
+  console.log(customer);
+  $.ajax({
+    type: "POST",
+    async: false,
+    dataType: "json",
+    data: customer,
+    url: "/CustomerSignUp"
   });
-});
+}
+
+function ownerSignUp(data) {
+  var name = data.name.value;
+  var username = data.username.value;
+  var partyRoomName = data.partyRoomName.value;
+  var email = data.email.value;
+  var password = data.password.value;
+  var phone = data.phone.value;
+  var owner = {
+    "name": name,
+    "username": username,
+    "partyRoomName": partyRoomName,
+    "email": email,
+    "password": password,
+    "phone": phone
+  }
+  console.log(owner);
+  $.ajax({
+    type: "POST",
+    async: false,
+    dataType: "json",
+    data: owner,
+    url: "/OwnerSignUp"
+  });
+}
