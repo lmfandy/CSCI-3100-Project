007370e06365446e11bc2dc4f3e8c9e439d562ff
jacky310 <jackyng310@gmail.com>
2020-04-17 00:57:30 +0800

    insert section addPartyRoomPage in ownerSignup.html

--- Server/website/js/ownerSignup.js
+++ Server/website/js/ownerSignup.js
@@ -1,24 +1,31 @@
 $(function (){
+  var ownerData = "";
+
   $("#ownerSignUpForm").submit(function (e) {
     var form = $(this);
     var url = form.attr('action');
-    $.ajax({
-      type: "POST",
-      async: false,
-      data: form.serialize(),
-      url: url
-    })
-    .done(res => {
-      if (res == "SignupSuccess") {
-        $("#signupSuccessPage").show();
-        $("#ownerCreatePage").hide();
-      }
-      form[0].reset();
-    })
-    .fail((jqXHR, textStatus, err) => {
-      alert(err);
-      form[0].reset();
-    });
+    ownerData = form.serialize();
+    console.log(ownerData);
+
+    $("#addPartyRoomPage").show();
+    $("#ownerCreatePage").hide();
+    // $.ajax({
+    //   type: "POST",
+    //   async: false,
+    //   data: form.serialize(),
+    //   url: url
+    // })
+    // .done(res => {
+    //   if (res == "SignupSuccess") {
+    //     $("#signupSuccessPage").show();
+    //     $("#ownerCreatePage").hide();
+    //   }
+    //   form[0].reset();
+    // })
+    // .fail((jqXHR, textStatus, err) => {
+    //   alert(err);
+    //   form[0].reset();
+    // });
     e.preventDefault();
   });
 });
