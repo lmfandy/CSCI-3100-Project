d1c974669e3c908732f93df20cdf84706754a284
But Chi Hin <chihinbut@gmail.com>
2020-04-17 23:57:31 +0800

    testing addCard function added

--- Server/website/js/index.js
+++ Server/website/js/index.js
@@ -33,7 +33,8 @@ $(function () {
       .done(res => {
         alert(res);
         if (res == "SearchSuccess") {
-          $("html").animate({ scrollTop: $(searchResult).offset().top }, 600);
+
+          $("html").animate({ scrollTop: $("#scrollTo").offset().top }, 600);
         }
       })
       .fail((jqXHR, textStatus, err) => {
@@ -42,4 +43,43 @@ $(function () {
     e.preventDefault();
   });
 
+  $("#addCard").click(() => {
+    $("#searchResult").append(createCard("images/card-img.png",
+      "Party Room No.1",
+      "This is a longer card with supporting text below as a natural lead-in to additional content." +
+      "This content is a little bit longer.",
+      20, "CUHK", "FREE"
+    ));
+  });
+
 });
+
+function createCard(img, title, description, capacity, location, price) {
+  let cardContainer = $("<div class='col-lg-4'></div>");
+  let card = $("<div class='m-2 card'></div>");
+
+  let cardImg = $("<img class='card-img-top'></img>");
+  cardImg.attr("src", img);
+  cardImg.attr("alt", "Card image cap");
+  cardImg.attr("width", "256px");
+  cardImg.attr("height", "200px");
+
+  let cardBody = $("<div class='card-body'></div>");
+
+  let cardTitle = $("<h5 class='card-title'></h5>").text(title);
+  let cardDescription = $("<p class='card-text'></p>").text(description);
+
+  let cardDetail = $("<ul class='list-group list-group-flush'></ul>");
+  let cardCapacity = $("<li class='list-group-item'></li>").text("Capacity: " + capacity);
+  let cardLocation = $("<li class='list-group-item'></li>").text("Location: " + location);
+  let cardPrice = $("<li class='list-group-item'></li>").text("Price: " + price);
+  cardDetail.append(cardCapacity, cardLocation, cardPrice);
+
+  let cardButton = $("<div class='card-body'><button type='button' class='btn btn-success'>Book it!</button></div>");
+
+  cardBody.append(cardTitle, cardDescription, cardDetail, cardButton);
+  card.append(cardImg, cardBody);
+  cardContainer.append(card);
+
+  return cardContainer;
+}
